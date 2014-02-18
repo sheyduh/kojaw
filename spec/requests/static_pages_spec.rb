d56c4eba45d76f9afe7901894096635d21cbb7af
Sheyda <sdemooei@gmail.com>
2014-02-18 14:03:48 -0800

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,60 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  describe "Home page" do
+    before { visit root_path }
+    let(:heading)    { 'Kojaw' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
+  end
+
+  describe "Help page" do
+    .
+    .
+    .
+  end
+
+  describe "About page" do
+    .
+    .
+    .
+  end
+
+  describe "Contact page" do
+    .
+    .
+    .
+  end
+
+  describe "index" do
+
+    let(:user) { FactoryGirl.create(:user) }
+    it { should have_title('All users') }
+    it { should have_content('All users') }
+
+    describe "pagination" do
+
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
+      end
+    end
+  end
+  
+end

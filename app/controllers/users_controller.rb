472e1585b5e76df32bd4ddbd82b29939c67d48ec
Sheyda <sdemooei@gmail.com>
2014-03-03 11:15:20 -0800

    Fix heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,6 @@ class UsersController < ApplicationController
   def create
       @user = User.new(user_params)
       if @user.save
-        Notifier.send_signup_email(@user).deliver
         sign_in @user
         flash[:success] = "Welcome to the Sample App!"
         redirect_to @user

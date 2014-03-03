68a953ecc644d7469e252442712c4e52f6d17605
Sheyda <sdemooei@gmail.com>
2014-03-02 18:24:14 -0800

    broke heroku again

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Kojaw::Application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => 'sheyxeare',
-  :password => 'tehran1990',
-  :domain => 'kojaw.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}

5b803d9503ea5f528124a22467e57ed4bea2d4c2
Sheyda <sdemooei@gmail.com>
2014-03-03 11:36:56 -0800

    omg

--- app/mailers/user_mailer.rb
+++
@@ -1,5 +0,0 @@
-class UserMailer < ActionMailer::Base
-def registration_confirmation(user)
-  mail(:to => user.email, :subject => "Registered", :from => "sheyda@kojaw.com")
-end
-end
--- config/initializers/setup_mail.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 465,
-  :domain               => "kojaw.com",
-  :user_name            => "sheyda@kojaw.com",
-  :password             => "tehran1990",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
